351d49fc725aa6906129547d732905fea261ee33
Guilherme Viana <guibvianaribeiro@gmail.com>
2024-11-24 13:01:32 -0300

    Adição do usuário na home screen

--- lib/modules/home_screen/home_screen.dart
+++ lib/modules/home_screen/home_screen.dart
@@ -1,19 +1,48 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 import '../../controllers/categoria_controller.dart';
 import '../pictures_screen/pictures_screen.dart';
+import '../login_screen/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   final CategoriaController controller = Get.put(CategoriaController());
+
+  // Estados reativos para o perfil
+  final RxString profileImagePath = ''.obs; // Caminho da imagem de perfil
+  final RxString userName = 'Usuário'.obs; // Nome escolhido pelo usuário
+  final RxString userEmail = ''.obs; // Email do Firebase
 
   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Obter o email do Firebase Authentication ao inicializar
+    _fetchUserEmail();
+
     return Scaffold(
-      appBar: AppBar(title: Text('Categorias')),
+      appBar: AppBar(
+        title: Text('Categorias'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: _logout,
+            tooltip: 'Sair',
+          ),
+        ],
+      ),
       body: Column(
         children: [
+          // Seção do perfil
+          _UserProfile(
+            profileImagePath: profileImagePath,
+            userName: userName,
+            userEmail: userEmail,
+            onLogout: _logout,
+          ),
+          // Lista de categorias
           Expanded(
             child: Obx(() {
               if (controller.categorias.isEmpty) {
@@ -45,7 +74,6 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                     onTap: () {
-                      // Navega para a tela de fotos com o ID e nome da categoria
                       Get.to(() => PicturesScreen(categoria: categoria));
                     },
                   );
@@ -53,10 +81,32 @@ class HomeScreen extends StatelessWidget {
               );
             }),
           ),
-          _AdicionarCategoria(controller: controller),
+          _AdicionarCategoria(controller),
         ],
       ),
     );
+  }
+
+  // Busca o email do usuário autenticado no Firebase
+  void _fetchUserEmail() {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      userEmail.value = user.email ?? 'Sem email disponível';
+    }
+  }
+
+  // Realiza o logout do usuário
+  Future<void> _logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Get.offAll(() => LoginScreen());
+    } catch (e) {
+      Get.snackbar(
+        'Erro',
+        'Falha ao realizar logout: $e',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
   }
 
   void _mostrarDialogoEditar(BuildContext context, Categoria categoria) {
@@ -121,26 +171,85 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _AdicionarCategoria extends StatefulWidget {
-  final CategoriaController controller;
+class _UserProfile extends StatelessWidget {
+  final RxString profileImagePath;
+  final RxString userName;
+  final RxString userEmail;
+  final VoidCallback onLogout;
 
-  _AdicionarCategoria({required this.controller});
-
-  @override
-  _AdicionarCategoriaState createState() => _AdicionarCategoriaState();
-}
-
-class _AdicionarCategoriaState extends State<_AdicionarCategoria> {
-  final TextEditingController _nomeController = TextEditingController();
-
-  @override
-  void dispose() {
-    _nomeController.dispose();
-    super.dispose();
-  }
+  _UserProfile({
+    required this.profileImagePath,
+    required this.userName,
+    required this.userEmail,
+    required this.onLogout,
+  });
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Obx(() {
+            return GestureDetector(
+              onTap: () => _selecionarImagem(),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundImage: profileImagePath.isNotEmpty
+                    ? FileImage(File(profileImagePath.value))
+                    : AssetImage('assets/default_avatar.png') as ImageProvider,
+                child: profileImagePath.isEmpty
+                    ? Icon(Icons.camera_alt, size: 30, color: Colors.white)
+                    : null,
+              ),
+            );
+          }),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Obx(() => Text(
+                      userName.value,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                Obx(() => Text(
+                      userEmail.value,
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    )),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: onLogout,
+            tooltip: 'Sair',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _selecionarImagem() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      profileImagePath.value = pickedFile.path;
+    }
+  }
+}
+
+class _AdicionarCategoria extends StatelessWidget {
+  final CategoriaController controller;
+
+  const _AdicionarCategoria(this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _nomeController = TextEditingController();
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -155,7 +264,7 @@ class _AdicionarCategoriaState extends State<_AdicionarCategoria> {
             icon: Icon(Icons.add),
             onPressed: () {
               if (_nomeController.text.isNotEmpty) {
-                widget.controller.adicionarCategoria(_nomeController.text);
+                controller.adicionarCategoria(_nomeController.text);
                 _nomeController.clear();
               } else {
                 Get.snackbar(
